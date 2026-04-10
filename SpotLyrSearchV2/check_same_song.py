1ccacf2ba07a009a9c72d5e56cd81a008e2046c9
Kllickon <karan.maniar03@gmail.com>
2026-04-10 10:10:27 +0100

    Delete SpotLyrSearchV2 directory
    I did not mean for everything to be in a folder

--- SpotLyrSearchV2/check_same_song.py
+++
@@ -1,9 +0,0 @@
-import get_song_artist
-import time
-
-def check_song_once() -> bool:
-    old_song_artist_tuple = get_song_artist.main()
-    while get_song_artist.main() == old_song_artist_tuple:
-        time.sleep(1)
-        print(old_song_artist_tuple)
-    return True
