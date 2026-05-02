7182a3231427e5403895e8319f2ac7ec95b4e7d5
Kllickon <karan.maniar03@gmail.com>
2026-05-02 12:37:54 +0100

    got rid of musixmatch stuff. wasnt working and i dont have time rn to figure it out

--- get_lyrics.py
+++ get_lyrics.py
@@ -2,12 +2,22 @@ import requests
 
 def search_keys(data) -> int:
     for k, v in data.items():
-        if k == "message":
+        if k == "message" or k == "":
             return 0
         elif k == "lyrics":
             return 1
     return 2
 
+def remove_spaces(name: str) -> str:
+    temp_list = name.split(" ")
+    x = ""
+    i = 0
+
+    for i in temp_list:
+        x += i
+    
+    return x
+    
 def get_lyrics(song: str, artist: str) -> str:
     if artist == "Spotify" or artist == "Spotify Free" or artist == "Spotify Premium":
         lyrics = "No song is currently playing"
@@ -18,24 +28,29 @@ def get_lyrics(song: str, artist: str) -> str:
         # print(URL)
         response_code = requests.get(URL)
         # print(response_code)
+        x = response_code.json()
+        # print(x)
         data = response_code.json()["data"]
         # print(data)
 
         check_data = search_keys(data)
 
         if check_data == 0:
-            URL = f"https://lyrics.lewdhutao.my.eu.org/v2/musixmatch/lyrics?title={song}&artist={artist}"
-            # print(URL)
-            response_code = requests.get(URL)
-            data = response_code.json()["data"]
+            # URL = f"https://lyrics.lewdhutao.my.eu.org/v2/musixmatch/lyrics?title={song}&artist={artist}"
+            # # print(URL)
+            # response_code = requests.get(URL)
+            # # x = response_code.json()
+            # # print(x)
+            # data = response_code.json()["data"]
 
-            check_data = search_keys(data)
-            if check_data == 0:
-                lyrics = f"The lyrics for {song} by {artist} were not found"
-            elif check_data == 1:
-                lyrics = data["lyrics"]
-            elif check_data == 2:
-                lyrics = "i dont know went wrong"
+            # check_data = search_keys(data)
+            # if check_data == 0:
+            #     lyrics = f"The lyrics for {song} by {artist} were not found"
+            # elif check_data == 1:
+            #     lyrics = data["lyrics"]
+            # elif check_data == 2:
+            #     lyrics = "i dont know went wrong"
+            lyrics = f"The lyrics for {song} by {artist} were not found"
         elif check_data == 1:
             lyrics = data["lyrics"]
         elif check_data == 2:
